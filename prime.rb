dc26b9431a1b9a3043043881df0af86226c14a26
Natalie Dean <ndean87@gmail.com>
2017-08-15 19:43:48 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
-# Add  code here!
+def prime?(integer)
+  if integer < 2
+    return false
+  end
+  prime_array = (2...integer).to_a
+  for i in prime_array
+    if integer % i == 0
+      return false
+    end
+  end
+  true
+end
+
+
+
+# make a range from 2 - integer
+# test whether the integer is divisible by any of the numbers in the array by looping over each one
+# if the i % integer == 0 then the method returns false
+#   if not return true
